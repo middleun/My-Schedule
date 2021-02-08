fe94b603e59204e1bcc289d174b93af212b36491
middleun <eunallaco@gmail.com>
2021-02-08 12:42:23 +0900

    210208_스케쥴 프로젝트 메인 페이지 헤더 완료

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Schedule</title>
+
+    <!-- reset css link -->
+    <link rel="stylesheet" href="/myschedule/css/reset.css"/>
+
+    <!-- style css link -->
+    <link rel="stylesheet" href="/myschedule/css/style.css"/>    
+</head>
+
+<body>
+    <!-- all contents are wrapped inside wrap -->
+    <div class="wrap">
+        <header>
+            <div class="center headerWrap">
+                <span class="hollow"></span>
+                <h1>Schedule Dashboard</h1>
+                <div class="mIcon">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+    
+        </header>
+
+        <!-- Container -->
+        <div class="container"></div>
+        
+    </div>
+    
+</body>
+</html>
